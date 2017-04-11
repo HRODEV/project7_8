b52c0b8dd22f872e6cf2e84351b01efaa71ddcd0
Thom Overhand <thomoverhand@145-24-234-207.wlan.hro.nl>
2017-04-11 13:53:43 +0200

    First commit, test.

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+	"path"
+)
+
+type Book struct {
+	Title  string `json:"title"`
+	Author string `json:"author"`
+}
+
+func main() {
+	http.HandleFunc("/", ShowBooks)
+	http.ListenAndServe(":8080", nil)
+}
+
+func ShowBooks(w http.ResponseWriter, r *http.Request) {
+	book := Book{"A song of Ice and Fire", "Kanye West"}
+
+	fp := path.Join("templates", "index.html")
+	templ, err := template.ParseFiles(fp)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if err := templ.Execute(w, book); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
