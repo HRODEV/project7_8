387e308e399a94b61bbe675128f2d1e5d70325ae
Barld Boot <bakeds@gmail.com>
2017-06-28 23:55:09 +0200

    added first complete test

--- dbActions/userActions_test.go
+++ dbActions/userActions_test.go
@@ -6,19 +6,28 @@ import (
 	"github.com/HRODEV/project7_8/models"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
-	"log"
 )
 
+var db *gorm.DB
+var initialized bool = false
+
+func init() {
+	if !initialized {
+		dbLocation := ":memory:" //"./db/declarations.sqlite"
+		db, _ = gorm.Open("sqlite3", dbLocation)
+		db.LogMode(true)
+		db.AutoMigrate(&models.User{}, &models.Declaration{}, &models.Receipt{}, &models.Project{}, &models.DeclarationStatus{})
+	}
+	initialized = true
+}
+
 func TestCreateUser(t *testing.T) {
-	dbLocation := ":memory:" //"./db/declarations.sqlite"
-	db, _ := gorm.Open("sqlite3", dbLocation)
-	defer db.Close()
-	db.LogMode(true)
-	db.AutoMigrate(&models.User{}, &models.Declaration{}, &models.Receipt{}, &models.Project{}, &models.DeclarationStatus{})
+	newUser := models.User{Email: "barld@barld.nl", FirstName: "Barld", LastName: "Boot", Password: "Secret"}
+	CreateUser(&newUser, db)
 
-	CreateUser(&models.User{Email: "barld@barld.nl", FirstName: "Barld", LastName: "Boot", Password: "Secret"}, db)
-
-	var users []models.User
-	db.Find(&users)
-	log.Print(users)
+	var lastUser models.User
+	db.Last(&lastUser)
+	if lastUser != newUser {
+		t.Errorf("the last user should be %#v but was %#v", newUser, lastUser)
+	}
 }
