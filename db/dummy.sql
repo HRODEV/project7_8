e9875cba4ba1392634246f2078736de31a52f9f5
Niels van der Veer (n9iels) <n.i.vdveer@gmail.com>
2017-06-28 10:10:35 +0200

    #16 Upload a receipt via multipart/content

--- db/dummy.sql
+++ db/dummy.sql
@@ -6,18 +6,18 @@ INSERT INTO Project (`Name`) VALUES
   ('Super awesome App'), ('Declaration App');
 
 INSERT INTO Receipt (`ImagePath`, `Data`) VALUES
-  ('path/to/image', ''), ('path/to/image', '');
+  ('path/to/image', ''), ('path/to/image', ''), ('path/to/image', ''), ('path/to/image', '');
 
 INSERT INTO Declaration (`Title`, `TotalPrice`, `VATPrice`, `Date`, `Description`, `ProjectID`, `StoreName`, `ReceiptID`, `UserID`) VALUES
-  ('Lunch', 19.99, 4.1979, '2017-07-09 12:32:00', 'Lunch voor onderweg', 1, 'Albert Heijn', 1, 1);
+  ('Lunch', 19.99, 4.1979, '2017-05-09 12:32:00', 'Lunch voor onderweg', 1, 'Albert Heijn', 1, 1);
 INSERT INTO Declaration (`Title`, `TotalPrice`, `VATPrice`, `Date`, `Description`, `ProjectID`, `StoreName`, `ReceiptID`, `UserID`) VALUES
   ('Laptop', 799.00, 167.79, '2017-05-09 12:32:00', 'Nieuwe laptop', 1, 'Alternate', 1, 1);
 INSERT INTO Declaration (`Title`, `TotalPrice`, `VATPrice`, `Date`, `Description`, `ProjectID`, `StoreName`, `ReceiptID`, `UserID`) VALUES
-  ('Dinner', 25.00, 167.79, '2017-05-09 12:32:00', 'Lekker avond eten', 1, 'Dönner Company', 1, 1);
+  ('Dinner', 25.00, 167.79, '2017-06-09 12:32:00', 'Lekker avond eten', 2, 'Dönner Company', 2, 2);
 
 
 INSERT INTO DeclarationStatus (`Status`, `DateModifed`, `DeclarationID`, `ModifiedByUserId`) VALUES
-  ('pending', '2017-07-09 12:32:00', 1, 1);
+  ('pending', '2017-05-09 12:32:00', 1, 1), ('pending', '2017-06-09 12:32:00', 2, 2);
 
 INSERT INTO UserProject VALUES
-  (1, 1);
+  (1, 1), (2, 2);
--- routes/receipt.go
+++ routes/receipt.go
@@ -5,12 +5,11 @@ import (
 	"github.com/HRODEV/project7_8/models"
 	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
+	"io"
 	"net/http"
+	"os"
 	"strconv"
 )
-
-type Receipt struct {
-}
 
 func ReceiptIdGet(w http.ResponseWriter, r *http.Request, db *gorm.DB) {
 	// Get request url parameters
@@ -46,6 +45,42 @@ func ReceiptIdImageGet(w http.ResponseWriter, r *http.Request, db *gorm.DB) {
 }
 
 func ReceiptPost(w http.ResponseWriter, r *http.Request, db *gorm.DB) {
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Not implemented yet"))
+	err := r.ParseMultipartForm(100000)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	//get a ref to the parsed multipart form
+	m := r.MultipartForm
+
+	//get the *fileheaders
+	files := m.File["image"]
+
+	for i, _ := range files {
+		//for each fileheader, get a handle to the actual file
+		file, err := files[i].Open()
+		defer file.Close()
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		//create destination file making sure the path is writeable.
+		dst, err := os.Create("/home/niels/" + files[i].Filename)
+		defer dst.Close()
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		//copy the uploaded file to the destination file
+		if _, err := io.Copy(dst, file); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
 }
